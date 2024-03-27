194c5d67d362e8fe9cc5ed9d2b23008421670fec
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-03-27 16:00:24 +0300

    feat: solved task (8 - logger)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -186,8 +186,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function res(...args) {
+    const argument = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argument}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argument}) ends`);
+    return result;
+  };
 }
 
 /**
