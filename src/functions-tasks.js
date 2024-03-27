59a2faa100593ab770861e629416caa52ce8d1ad
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-03-27 12:19:40 +0300

    feat: solved task (6 - memoize)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -119,8 +119,18 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+  let cachedRes = null;
+
+  return function memoizer() {
+    if (!cache) {
+      cachedRes = func();
+      cache = true;
+      return cachedRes;
+    }
+    return cachedRes;
+  };
 }
 
 /**
